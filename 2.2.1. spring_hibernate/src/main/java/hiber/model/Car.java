2003e41a02a09d908b27a3e8f676ebd04f54b99d
KiladzeGM <kiladzegm@gmail.com>
2022-10-04 02:45:01 +0500

    First

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,48 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "series")
+    private int series;
+
+    @Column(name = "model")
+    private String model;
+
+    public Car() {
+    }
+
+    public Car(int series, String model) {
+        this.series = series;
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "series='" + series + '\'' +
+                ", model='" + model + '\'';
+    }
+}
